203c9ee9ed8e81603922f72a7c599477a63c0ab4
Temich06 <artemfedorow06@gmail.com>
2024-12-06 03:50:31 +0900

    NIGHT

--- evergreen/page/urls.py
+++ evergreen/page/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 import page.views
 
 urlpatterns = [
-    path('', page.views.index),
+    path('', page.views.main),
+    path('index', page.views.index),
+    path('library', page.views.library),
+    path('olonho', page.views.olonho),
 ]
--- evergreen/page/views.py
+++ evergreen/page/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'page/index.html')
+
+
+def main(request):
+    return render(request, 'page/main.html')
+
+def library(request):
+    return render(request, 'page/library.html')
+
+def olonho(request):
+    return render(request, 'page/olonho.html')
